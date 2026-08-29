e58d5a31aef72ad1e6c77f7cc920090ed433661c
AKASHSHAKTHIRT <151288849+AKASHSHAKTHIRT@users.noreply.github.com>
2026-08-29 17:14:42 +0530

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+import random
+
+app = Flask(__name__)
+
+quotes = [
+    "The only way to do great work is to love what you do.",
+    "Believe you can and you're halfway there.",
+    "Success is not final, failure is not fatal.",
+    "It always seems impossible until it's done.",
+    "Dream big and dare to fail."
+]
+
+
+@app.route("/quote", methods=["GET"])
+def get_quote():
+    quote = random.choice(quotes)
+
+    return jsonify({
+        "quote": quote
+    })
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
